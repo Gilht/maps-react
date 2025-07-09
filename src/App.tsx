84fea4e4be538a4bea4fee510a95f7320c75392a
Gilht <ghdzt14@hotmail.com>
2025-07-08 21:36:55 -0600

    work progress

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <header>Hola mundo</header>
+    </div>
+  )
+}
+
+export default App
