4508fd3a2d93269d0dc17c37f4a5dc4824d2cefc
Gilht <ghdzt14@hotmail.com>
2025-07-14 19:16:54 -0600

    adding functionaly to show all places in a list below of search baar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,5 @@
-export { SearchBar } from "./SearchBar";
-
-export { ReactLogo } from "./ReactLogo";
-
 export { BtnMyLocation } from "./BtnMyLocation";
-
 export { MapView } from "./MapView";
-
-
-
+export { ReactLogo } from "./ReactLogo";
+export { SearchBar } from "./SearchBar";
+export { SearchResults } from "./SearchResults";
--- src/context/places/PlacesProvider.tsx
+++ src/context/places/PlacesProvider.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useReducer } from "react";
+import searchApi, { apiKey } from "../../apis/searchApi";
 import { getUserLocation } from "../../helpers/getUserLocation";
+import { Feature, PlacesResponse } from "../../interfaces/places";
 import { PlacesContext } from "./PlacesContext";
 import { placesReducer } from "./placesReducer";
 
@@ -12,11 +14,15 @@ interface Props {
 export interface PlacesState{
     isLoading: boolean;
     userLocation?: [number, number];
+    isLoadingPlaces: boolean;
+    places: Feature[];
 }
 
 const INITIAL_STATE: PlacesState = {
     isLoading: true,
-    userLocation: undefined
+    userLocation: undefined,
+    isLoadingPlaces: false,
+    places: []
 }
 
 export const PlacesProvider = ({children}: Props) => {
@@ -28,9 +34,27 @@ export const PlacesProvider = ({children}: Props) => {
         .then(lngNlat => dispatch({type: 'setUserLocation', payload: lngNlat})); 
     }, [])
 
+    const searchPlacesByTerm = async (query: string)  => {
+        if(query.length === 0) return []
+
+        dispatch({ type: 'setLoadingPlaces'});
+
+        const resp = await searchApi.get<PlacesResponse>(`/${encodeURIComponent(query)}.json`,{
+            params: {
+                proximity: state.userLocation?.join(','),
+                key: apiKey
+            }
+        });
+        
+        dispatch({ type: 'setPlaces', payload: resp.data.features});
+
+        return resp.data.features;
+    }
+
     return (        
         <PlacesContext.Provider value={{
             ...state,
+            searchPlacesByTerm
         }}>
             {children}
         </PlacesContext.Provider>
